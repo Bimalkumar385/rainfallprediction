9da08533d3bf83e72b1778855457827a360684b5
Bimalkumar385 <83906093+Bimalkumar385@users.noreply.github.com>
2021-09-29 22:40:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,31 +12,31 @@ def index():  # put application's code here
 def predict():
         b = []
         if request.method == "POST":
-            Year = request.form['year']
+            Year = int(request.form['year'])
 
-            Month = request.form['month']
+            Month = int(request.form['month'])
 
-            Day = request.form['day']
+            Day = int(request.form['day'])
 
-            MinTemp = request.form['mintemp']
+            MinTemp = float(request.form['mintemp'])
 
-            MaxTemp = request.form['maxtemp']
+            MaxTemp = float(request.form['maxtemp'])
 
-            Humidity9am = request.form['humidity9']
+            Humidity9am = float(request.form['humidity9'])
 
-            Humidity3pm = request.form['humidity3']
+            Humidity3pm = float(request.form['humidity3'])
 
-            Pressure9am = request.form['pressure9']
+            Pressure9am = float(request.form['pressure9'])
 
-            Pressure3pm = request.form['pressure3']
+            Pressure3pm = float(request.form['pressure3'])
 
-            Temp9am = request.form['temp9']
+            Temp9am = float(request.form['temp9'])
 
-            Temp3pm = request.form['temp3']
+            Temp3pm = float(request.form['temp3'])
 
-            WindSpeed9am = request.form['wind9']
+            WindSpeed9am = float(request.form['wind9'])
 
-            WindSpeed3pm = request.form['wind3']
+            WindSpeed3pm = float(request.form['wind3'])
 
             b.extend([ Year, Month, Day, MinTemp, MaxTemp, Humidity9am, Humidity3pm,
                       Pressure9am, Pressure3pm, Temp9am, Temp3pm, WindSpeed9am, WindSpeed3pm])
